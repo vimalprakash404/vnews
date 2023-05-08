a74092676c39c3760fd81ad43b695247e01e2147
vimal prakash <vimalprakash332@gmail.com>
2023-05-08 22:23:21 +0530

    view removed News form

--- Author/form.py
+++ Author/form.py
@@ -1,25 +1,29 @@
 from django import forms
 from .models import *
-from django.forms.widgets import DateInput,TextInput
+from django.forms.widgets import DateInput, TextInput
+
+
 class NewsForm(forms.ModelForm):
     class Meta:
         model = News
-        fields="__all__"
-        widgets={
-            'date':DateInput(attrs={'type':"date"}),
-            "content":TextInput(attrs={"hidden":"","required" :False})
+        exclude = ['views']
+        widgets = {
+            'date': DateInput(attrs={'type': "date"}),
+            "content": TextInput(attrs={"hidden": "", "required": False})
         }
-    def __init__(self,*args,**kwargs):
-        super(NewsForm,self).__init__(*args,**kwargs)
+
+    def __init__(self, *args, **kwargs):
+        super(NewsForm, self).__init__(*args, **kwargs)
         for i in self.fields.values():
-            i.widget.attrs['class']="form-control form-control-user"
-        
+            i.widget.attrs['class'] = "form-control form-control-user"
+
+
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        fields="__all__"
-        
-    def __init__(self,*args,**kwargs):
-        super(CategoryForm,self).__init__(*args,**kwargs)
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(CategoryForm, self).__init__(*args, **kwargs)
         for i in self.fields.values():
-            i.widget.attrs['class']="form-control"
+            i.widget.attrs['class'] = "form-control"
