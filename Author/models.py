5e57a849d25fd4842d459dd37936141171def8bc
vimal prakash <vimalprakash332@gmail.com>
2023-05-09 23:51:19 +0530

    view bug fixed

--- Author/models.py
+++ Author/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Category(models.Model):
-    name=models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
-    
+
 
 class News(models.Model):
-    title=models.CharField(max_length=50)
-    views=models.IntegerField()
-    image=models.ImageField(upload_to="news", height_field=None, width_field=None, max_length=None)
-    Category=models.ForeignKey(Category, on_delete=models.CASCADE)
-    place=models.CharField(null=True, max_length=50)
-    date=models.DateField(auto_now=False, auto_now_add=False,null=True)
-    content=models.CharField(max_length=100000)
+    title = models.CharField(max_length=50)
+    views = models.IntegerField(default=0)
+    image = models.ImageField(
+        upload_to="news", height_field=None, width_field=None, max_length=None)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    place = models.CharField(null=True, max_length=50)
+    date = models.DateField(auto_now=False, auto_now_add=False, null=True)
+    content = models.CharField(max_length=100000)
